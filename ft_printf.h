dff7c3dd6b1c2659f770df57924b6e6d6c4f5368
ebin-ahm <edieusdi@gmail.com>
2026-01-22 17:21:27 +0800

    Format alphabetically function prototypes to ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -15,15 +15,17 @@
 
 # include <stdarg.h>
 # include <unistd.h>
+# include <stddef.h>
 
 int	ft_printf(const char *format, ...);
+int	ft_write(const void *buf, size_t n);
 
 int	print_char(int c);
-int	print_string(char *s);
-int	print_int(int n);
-int	print_unsigned(unsigned int n);
 int	print_hex(unsigned int n, int uppercase);
-int	print_pointer(void *ptr);
+int	print_int(int n);
 int	print_percent(void);
+int	print_pointer(void *ptr);
+int	print_string(char *s);
+int	print_unsigned(unsigned int n);
 
 #endif
